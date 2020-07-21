e61c00364eabb61010a3f474d4cd65b10b3a9839
Romeotamizh <romeotamizh@gmail.com>
2020-07-21 17:04:38 +0530

    Adruino

--- app/src/main/java/com/tech4lyf/sbsrvending/PayActivity.java
+++ app/src/main/java/com/tech4lyf/sbsrvending/PayActivity.java
@@ -9,17 +9,25 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.Message;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -29,14 +37,21 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
 import com.shreyaspatil.EasyUpiPayment.EasyUpiPayment;
 import com.shreyaspatil.EasyUpiPayment.listener.PaymentStatusListener;
 import com.shreyaspatil.EasyUpiPayment.model.TransactionDetails;
 
+import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.Arrays;
+import java.util.EnumSet;
 
-public class PayActivity extends AppCompatActivity {
+public class PayActivity extends AppCompatActivity implements SerialListener, ServiceConnection {
+
+
     private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -59,18 +74,17 @@ public class PayActivity extends AppCompatActivity {
             }
         }
     };
-    private final ServiceConnection usbConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
-            usbService = ((UsbService.UsbBinder) arg1).getService();
-            usbService.setHandler(mHandler);
-        }
+    private int deviceId, portNum, baudRate;
+    private String newline = "\r\n";
 
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            usbService = null;
-        }
-    };
+    private TextView receiveText;
+
+    private UsbSerialPort usbSerialPort;
+    private SerialService service;
+    private boolean initialStart = true;
+    private Connected connected = Connected.False;
+    private BroadcastReceiver broadcastReceiver;
+    private ControlLines controlLines;
     private RecyclerViewAdapterPay recyclerViewAdapterPay;
     private RecyclerView recyclerViewPay;
     private TextView payTotal;
@@ -83,6 +97,18 @@ public class PayActivity extends AppCompatActivity {
     private String[] payProductPrice_X_Counts = new String[12];
     private UsbService usbService;
     private MyHandler mHandler;
+    private final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+            usbService = ((UsbService.UsbBinder) arg1).getService();
+            usbService.setHandler(mHandler);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            usbService = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,6 +119,12 @@ public class PayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pay);
         mHandler = new MyHandler(this);
 
+       /* setHasOptionsMenu(true);
+        setRetainInstance(true);
+        deviceId = getArguments().getInt("device");
+        portNum = getArguments().getInt("port");
+        baudRate = getArguments().getInt("baud");
+*/
         for (int i = 0; i < 12; i++) {
             if (MainActivity.a[i] != 0) {
                 payProductNames[x] = MainActivity.products[i].getName();
@@ -113,6 +145,15 @@ public class PayActivity extends AppCompatActivity {
 
             //R11198867
 
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    if (intent.getAction().equals(Constants.INTENT_ACTION_GRANT_USB)) {
+                        Boolean granted = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false);
+                        connect(granted);
+                    }
+                }
+            };
 
             payEditItems.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -255,6 +296,135 @@ public class PayActivity extends AppCompatActivity {
         return UPI.replace(" ", "+");
     }
 
+    @Override
+    public void onSerialConnect() {
+        status("connected");
+        connected = Connected.True;
+        controlLines.start();
+    }
+
+    @Override
+    public void onSerialConnectError(Exception e) {
+        status("connection failed: " + e.getMessage());
+        disconnect();
+    }
+
+    @Override
+    public void onSerialRead(byte[] data) {
+        receive(data);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder binder) {
+        service = ((SerialService.SerialBinder) binder).getService();
+        service.attach(this);
+        if (initialStart) {
+            initialStart = false;
+            PayActivity.this.runOnUiThread(this::connect);
+        }
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        service = null;
+    }
+
+    @Override
+    public void onSerialIoError(Exception e) {
+        status("connection lost: " + e.getMessage());
+        disconnect();
+    }
+
+    private void connect() {
+        connect(null);
+    }
+
+    private void connect(Boolean permissionGranted) {
+        UsbDevice device = null;
+        UsbManager usbManager = (UsbManager) PayActivity.this.getSystemService(Context.USB_SERVICE);
+        for (UsbDevice v : usbManager.getDeviceList().values())
+            if (v.getDeviceId() == deviceId)
+                device = v;
+        if (device == null) {
+            status("connection failed: device not found");
+            return;
+        }
+        UsbSerialDriver driver = UsbSerialProber.getDefaultProber().probeDevice(device);
+        if (driver == null) {
+            driver = CustomProber.getCustomProber().probeDevice(device);
+        }
+        if (driver == null) {
+            status("connection failed: no driver for device");
+            return;
+        }
+        if (driver.getPorts().size() < portNum) {
+            status("connection failed: not enough ports at device");
+            return;
+        }
+        usbSerialPort = driver.getPorts().get(portNum);
+        UsbDeviceConnection usbConnection = usbManager.openDevice(driver.getDevice());
+        if (usbConnection == null && permissionGranted == null && !usbManager.hasPermission(driver.getDevice())) {
+            PendingIntent usbPermissionIntent = PendingIntent.getBroadcast(PayActivity.this, 0, new Intent(Constants.INTENT_ACTION_GRANT_USB), 0);
+            usbManager.requestPermission(driver.getDevice(), usbPermissionIntent);
+            return;
+        }
+        if (usbConnection == null) {
+            if (!usbManager.hasPermission(driver.getDevice()))
+                status("connection failed: permission denied");
+            else
+                status("connection failed: open failed");
+            return;
+        }
+
+        connected = Connected.Pending;
+        try {
+            usbSerialPort.open(usbConnection);
+            usbSerialPort.setParameters(baudRate, UsbSerialPort.DATABITS_8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            SerialSocket socket = new SerialSocket(PayActivity.this.getApplicationContext(), usbConnection, usbSerialPort);
+            service.connect(socket);
+            // usb connect is not asynchronous. connect-success and connect-error are returned immediately from socket.connect
+            // for consistency to bluetooth/bluetooth-LE app use same SerialListener and SerialService classes
+            onSerialConnect();
+        } catch (Exception e) {
+            onSerialConnectError(e);
+        }
+    }
+
+    private void disconnect() {
+        connected = Connected.False;
+        controlLines.stop();
+        service.disconnect();
+        usbSerialPort = null;
+    }
+
+    private void send(String str) {
+        if (connected != Connected.True) {
+            Toast.makeText(PayActivity.this, "not connected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            SpannableStringBuilder spn = new SpannableStringBuilder(str + '\n');
+            spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            receiveText.append(spn);
+            byte[] data = Arrays.toString(MainActivity.a).getBytes();
+            service.write(data);
+        } catch (Exception e) {
+            onSerialIoError(e);
+        }
+    }
+
+    private void receive(byte[] data) {
+        receiveText.append(new String(data));
+    }
+
+    void status(String str) {
+        SpannableStringBuilder spn = new SpannableStringBuilder(str + '\n');
+        spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        receiveText.append(spn);
+    }
+
+    private enum Connected {False, Pending, True}
+
     private static class MyHandler extends Handler {
         private final WeakReference<PayActivity> mActivity;
 
@@ -279,5 +449,58 @@ public class PayActivity extends AppCompatActivity {
         }
     }
 
+    class ControlLines {
+        private static final int refreshInterval = 200; // msec
+
+        private Handler mainLooper;
+        private Runnable runnable;
+
+        ControlLines(View view) {
+            mainLooper = new Handler(Looper.getMainLooper());
+            runnable = this::run; // w/o explicit Runnable, a new lambda would be created on each postDelayed, which would not be found again by removeCallbacks
+
+
+        }
+
+        private void toggle(View v) {
+            ToggleButton btn = (ToggleButton) v;
+            if (connected != Connected.True) {
+                btn.setChecked(!btn.isChecked());
+                Toast.makeText(PayActivity.this, "not connected", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            String ctrl = "";
+
+        }
+
+        private void run() {
+            if (connected != Connected.True)
+                return;
+            try {
+                EnumSet<UsbSerialPort.ControlLine> controlLines = usbSerialPort.getControlLines();
+
+                mainLooper.postDelayed(runnable, refreshInterval);
+            } catch (IOException e) {
+                status("getControlLines() failed: " + e.getMessage() + " -> stopped control line refresh");
+            }
+        }
+
+        void start() {
+            if (connected != Connected.True)
+                return;
+            try {
+                EnumSet<UsbSerialPort.ControlLine> controlLines = usbSerialPort.getSupportedControlLines();
+
+                run();
+            } catch (IOException e) {
+                Toast.makeText(PayActivity.this, "getSupportedControlLines() failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        void stop() {
+            mainLooper.removeCallbacks(runnable);
+
+        }
+    }
 
 }
